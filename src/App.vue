6337271f097f92be5fb0c6b60422dec0058808fa
fulin.tang <tangtangdeyx@126.com>
2020-09-15 18:30:40 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <v-header />
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header.vue';
+export default {
+  components: {
+    'v-header': header
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+// vue-loader下的postcss帮我们做了CSS对各个浏览器的适应
+// CSS规范， 先写布局， 宽高接上， 最后是color等属性
+#app
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240,20,20)
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import goods from '@/components/goods/goods';
+import ratings from '@/components/ratings/ratings';
+import seller from '@/components/seller/seller';
+
+Vue.use(Router);
+
+export default new Router({
+  linkActiveClass: 'active', // 指定被激活的router为active
+  routes: [
+    {
+      path: '/',
+      name: 'goods',
+      redirect: '/goods'
+    }, {
+      path: '/goods',
+      name: 'goods1',
+      component: goods
+    }, {
+      path: '/ratings',
+      name: 'ratings',
+      component: ratings
+    }, {
+      path: '/seller',
+      name: 'seller',
+      component: seller
+    }
+  ]
+});
