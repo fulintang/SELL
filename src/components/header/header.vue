46192f2cb69c568b9dd267e1c120a5876137ed7c
fulin.tang <tangtangdeyx@126.com>
2020-09-22 09:09:57 +0800

    header更新 bulletin-title

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -14,7 +14,15 @@
           <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
+        <div v-if="seller.supports" class="support-count">
+          <span class="count">{{seller.supports.length + '个'}}</span>
+          <i class="icon-keyboard_arrow_right"></i>
+        </div>
       </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right" />
     </div>
   </div>
 </template>
@@ -36,9 +44,10 @@ export default {
 @import '../../common/stylus/mixin.styl'
 .header
   color: #fff
+  background: rgba(7, 17, 27, 0.5)
   .content-wrapper
+    position: relative
     padding: 24px 12px 18px 24px
-    background: rgba(7, 17, 27, 0.5)
     font-size: 0
     .avatar
       display: inline-block
@@ -89,4 +98,39 @@ export default {
         .text
           line-height: 12px
           font-size: 10px
+    .support-count
+      position: absolute
+      right: 12px
+      bottom: 16px
+      padding: 0 8px
+      height: 24px
+      line-height: 24px
+      border-radius: 14px
+      background-color: rgba(0, 0, 0, 0.2)
+      .count
+        font-size: 10px
+        vertical-align: top
+      .icon-keyboard_arrow_right
+        line-height: 24px
+        margin-left: 2px
+        font-size: 10px
+  .bulletin-wrapper
+    height: 28px
+    line-height: 28px
+    padding: 0 22px 0 12px
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
+    // font-size: 0
+    background-color: rgba(7, 17, 27, 0.2)
+    .bulletin-title
+      display: inline-block
+      width: 22px
+      height: 12px
+      bg-image('bulletin')
+      background-size: 100%
+      background-repeat: no-repeat
+    .bulletin-text
+      margin: 0 4px
+      font-size: 10px
 </style>
