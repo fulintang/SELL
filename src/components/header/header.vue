6e2e82a4d2b2aa067bfba197e06e0fa22b839500
fulin.tang <tangtangdeyx@126.com>
2020-09-29 19:26:22 +0800

    修改线条

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -221,6 +221,19 @@ export default {
           margin-top: 18px
           padding: 2px 0
           text-align: center
+        .title
+          display: flex
+          width: 80%
+          margin: 28px auto 24px auto
+          .line
+            flex: 1
+            position: relative
+            top: -6px
+            border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+          .text
+            padding: 0 12px
+            font-weight: 700
+            font-size: 14px
     .detail-close
       position: relative
       width: 32px
