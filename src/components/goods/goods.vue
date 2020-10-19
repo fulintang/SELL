d78bdcc62e97419b064cc62a8390a5155314cc96
fulin.tang <tangtangdeyx@126.com>
2020-10-19 19:50:54 +0800

    左侧菜单布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="(item,index) in goods" :key="index" class="menu-item">
+          <span class="text border-1px">
+            <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
     <div class="foods-wrapper"></div>
   </div>
 </template>
@@ -21,6 +30,7 @@ export default {
     };
   },
   created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     goodsApi.getlist().then(res => {
       if (res.data.errno === ERR_OK) {
         this.goods = res.data.data;
@@ -32,6 +42,7 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixin.styl'
 .goods
   background-color: red
   display: flex
@@ -44,6 +55,36 @@ export default {
     flex: 0 0 80px
     width: 80px
     background-color: #f3f5f7
+    .menu-item
+      display: table
+      margin: auto
+      width: 56px
+      min-height: 54px
+      line-height: 14px
+      .text
+        display: table-cell
+        width: 56px
+        vertical-align: middle
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 12px
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 100%
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
   .foods-wrapper
     flex: 1
     background-color: blue
